bcd2ddd71e490f613682b39524d3f11fc51a6b99
Qu Junping <qvjunping@gmail.com>
2024-08-18 10:32:02 +0800

    paging

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,19 +1,34 @@
 #![feature(abi_x86_interrupt)]
 #![no_std]
 #![no_main]
+extern crate alloc;
 
+use crate::memory::BootInfoFrameAllocator;
+use bootloader_api::config::Mapping;
+use bootloader_api::BootloaderConfig;
 use core::panic::PanicInfo;
+use x86_64::VirtAddr;
 
 mod framebuffer;
 mod interrupt;
 mod serial;
+mod memory;
+mod allocator;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("{}", info);
+    println!("{}", info);
+    hlt_loop();
 }
 
-bootloader_api::entry_point!(kernel_main);
+pub static BOOTLOADER_CONFIG: BootloaderConfig = {
+    let mut config = BootloaderConfig::new_default();
+    config.mappings.physical_memory = Some(Mapping::Dynamic);
+    config
+};
+
+bootloader_api::entry_point!(kernel_main,config = &BOOTLOADER_CONFIG);
 fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     framebuffer::init_display(boot_info.framebuffer.as_mut().unwrap());
     interrupt::init_idt();
@@ -22,7 +37,18 @@ fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     }
     x86_64::instructions::interrupts::enable();
 
+    let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset.into_option()
+        .expect("map-physical-memory config option must be enabled"));
+    let mut mapper = unsafe { memory::init(physical_memory_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_regions) };
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
     println!("Hi, This is INWOX OS{}", '!');
-    serial_println!("Hi, This is INWOX OS{}", '!');
-    loop {}
+    hlt_loop();
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
---
+++ kernel/src/memory.rs
@@ -0,0 +1,50 @@
+use bootloader_api::info::{MemoryRegionKind, MemoryRegions};
+use x86_64::structures::paging::{FrameAllocator, OffsetPageTable, PageTable, PhysFrame, Size4KiB};
+use x86_64::{PhysAddr, VirtAddr};
+
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_page_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_page_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+    use x86_64::registers::control::Cr3;
+    let (level_4_table_frame, _) = Cr3::read();
+    let phys = level_4_table_frame.start_address();
+    let virt = physical_memory_offset + phys.as_u64();
+
+    let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
+
+    &mut *page_table_ptr
+}
+
+pub struct BootInfoFrameAllocator {
+    memory_regions: &'static MemoryRegions,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(memory_regions: &'static MemoryRegions) -> Self {
+        BootInfoFrameAllocator {
+            memory_regions,
+            next: 0,
+        }
+    }
+
+    fn usable_frames(&self) -> impl Iterator<Item=PhysFrame> {
+        let regions = self.memory_regions.iter();
+        let usable_regions = regions.filter(|r| r.kind == MemoryRegionKind::Usable);
+        let addr_ranges = usable_regions.map(|r| r.start..r.end);
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
+}
